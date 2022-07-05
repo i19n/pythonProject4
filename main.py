bc70ca1107eb2b771195d68cc8c750bb6df1a66f
Иван <sigaga511@gmail.com>
2022-07-05 16:54:52 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+string=input('Выберите строку:')
+print('Вы сделали выбор:"{}"'.format(string))
+
+print('Нажмите ввод чтобы продолжить')
+input()
+
+n=int(input('Первое число:'))
+m=int(input('Второе число:'))
+print('{}+{}={}'.format(n,m,n+m))
